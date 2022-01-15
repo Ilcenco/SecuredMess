38af91348344950c5cb77b6031ece9bded77dbfb
Ilcenco <ilcenco.eugeniu@isa.utm.md>
2022-01-15 15:00:19 +0200

    Add project files.

---
+++ SecuredMess/wwwroot/js/SignInJs.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $("#logInForm").validate({
+        rules: {
+            userName: {
+                required: true,
+                minlength: 7,
+                maxlength: 22,
+            },
+            password: {
+                required: true,
+                minlength: 9,
+            }
+        }
+    });
+    $("#logInForm").submit(function (event) {
+        event.preventDefault();
+        if ($("#logInForm").valid()) {
+            SignInRequest();
+        }     
+    });
+
+});
+
+function SignInRequest() {
+
+    var formData = $("#logInForm").serializeArray();
+    console.log(formData);
+
+    $.ajax({
+        url: "https://localhost:8000/auth/sign-in",
+        method: "POST",
+        data: formData,
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            console.log(this.url);
+        }
+    });
+}
---
+++ SecuredMess/wwwroot/js/SignUpJS.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function () {
+    $("#signUpForm").validate({
+        rules: {
+            email: {
+                required: true,
+                minlength: 8,
+                maxlength: 30,
+            },
+            userName: {
+                required: true,
+                minlength: 7,
+                maxlength: 22,
+            },
+            password: {
+                required: true,
+                minlength: 9,
+            },
+            passwordConfirm: {
+                required: true,
+                equalTo: "#typePassword"
+            }
+        }
+    });
+    $("#signUpForm").submit(function (event) {
+        event.preventDefault();
+        if ($("#signUpForm").valid()) {
+            SignUpRequest();
+        }
+    });
+
+});
+
+function SignUpRequest() {
+
+    var formData = $("#signUpForm").serializeArray();
+    console.log(formData);
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8000/auth/sign-up',
+        crossDomain: true,
+        data: formData,
+        dataType: 'json',
+        success: function (responseData, textStatus, jqXHR) {
+
+            console.log(responseData);
+
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            alert('POST failed.');
+        }
+    });
+
+    //$.ajax({
+    //    type: "POST",
+    //    url: "https://localhost:8000/auth/sign-up",
+    //    dataType: "jsonp",
+    //    data: formData,
+    //    success: function (data) {
+    //        console.log(data);
+
+    //    },
+    //    error: function (xhr, status, error) {
+    //        console.log(error);
+
+    //    }
+    //});
+}
